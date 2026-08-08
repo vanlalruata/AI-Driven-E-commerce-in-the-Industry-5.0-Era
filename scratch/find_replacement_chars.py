63761c1c5c2dbd9d418ea8d76142fede5c38bd28
Vanlalruata Hnamte <vanlalruata.hnamte@gmail.com>
2026-08-08 09:37:37 +0530

    correction

--- scratch/find_replacement_chars.py
+++
@@ -1,6 +0,0 @@
-with open("manuscript/main.tex", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-
-for idx, line in enumerate(lines):
-    if "\uFFFD" in line:
-        print(f"Line {idx+1}: {repr(line)}")
